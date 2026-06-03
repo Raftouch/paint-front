f8658b7e4ae2e0bdc79ace2f177f4300b94be1b4
raftouch <rafanelkin@gmail.com>
2026-06-03 14:41:59 +0200

    feat(ui): render tool and action buttons

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,5 +1,21 @@
+import Button from "./Button";
+
 export default function Toolbar() {
   return (
-    <div className="h-[40px] bg-white flex items-center shadow-lg">Toolbar</div>
+    <div className="h-[40px] bg-white flex items-center justify-between shadow-lg p-4">
+      <div className="space-x-4 flex items-center">
+        <Button imageName="brush" />
+        <Button imageName="rect" />
+        <Button imageName="circle" />
+        <Button imageName="eraser" />
+        <Button imageName="line" />
+      </div>
+
+      <div className="space-x-4 flex items-center">
+        <Button imageName="undo" />
+        <Button imageName="redo" />
+        <Button imageName="save" />
+      </div>
+    </div>
   );
 }
