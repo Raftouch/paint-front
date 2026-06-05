842b93378d394e8288d0b27dd23e0975a23e0c17
raftouch <rafanelkin@gmail.com>
2026-06-05 17:27:33 +0200

    feat(canvas): initialize canvas and connect to mobx store

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,7 +1,20 @@
-export default function Canvas() {
+import { observer } from "mobx-react-lite";
+import { useEffect, useRef } from "react";
+import canvasState from "../store/canvasState";
+
+function Canvas() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    canvasState.setCanvas(canvas);
+  }, []);
+
   return (
     <div className="mt-5 flex justify-center p-4">
       <canvas
+        ref={canvasRef}
         width={800}
         height={600}
         className="w-full max-w-6xl bg-white border border-black"
@@ -9,3 +22,5 @@ export default function Canvas() {
     </div>
   );
 }
+
+export default observer(Canvas);
