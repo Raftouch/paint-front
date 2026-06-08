8c338d6b508a9b3faed3c78484fb8113b659aab5
raftouch <rafanelkin@gmail.com>
2026-06-08 19:20:29 +0200

    feat(toolbar): add brush tool selection via button click

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,21 +1,30 @@
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
 import Button from "./Button";
 
 export default function Toolbar() {
+  const handleClick = () => {
+    if (!canvasState.canvas) return;
+
+    toolState.setTool(new Brush(canvasState.canvas));
+  };
+
   return (
     <div className="h-[40px] bg-white flex items-center justify-between shadow-lg p-4">
       <div className="space-x-4 flex items-center">
-        <Button imageName="brush" />
-        <Button imageName="rect" />
+        <Button imageName="brush" onClick={handleClick} />
+        {/* <Button imageName="rect" />
         <Button imageName="circle" />
         <Button imageName="eraser" />
-        <Button imageName="line" />
+        <Button imageName="line" /> */}
         <input type="color" />
       </div>
 
       <div className="space-x-4 flex items-center">
-        <Button imageName="undo" />
+        {/* <Button imageName="undo" />
         <Button imageName="redo" />
-        <Button imageName="save" />
+        <Button imageName="save" /> */}
       </div>
     </div>
   );
