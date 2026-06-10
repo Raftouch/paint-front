b3231e316e431721191921a12a51f46bfad0d9fe
raftouch <rafanelkin@gmail.com>
2026-06-10 19:38:23 +0200

    feat(toolbar): add circle tool selection via button click

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,6 +1,7 @@
 import canvasState from "../store/canvasState";
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import Circle from "../tools/Circle";
 import Rect from "../tools/Rect";
 import Button from "./Button";
 
@@ -17,12 +18,18 @@ export default function Toolbar() {
     toolState.setTool(new Rect(canvasState.canvas));
   };
 
+  const handleCircleClick = () => {
+    if (!canvasState.canvas) return;
+
+    toolState.setTool(new Circle(canvasState.canvas));
+  };
+
   return (
     <div className="h-[40px] bg-white flex items-center justify-between shadow-lg p-4">
       <div className="space-x-4 flex items-center">
         <Button imageName="brush" onClick={handleBrushClick} />
         <Button imageName="rect" onClick={handleRectClick} />
-        {/* <Button imageName="circle" /> */}
+        <Button imageName="circle" onClick={handleCircleClick} />
         {/* <Button imageName="eraser" /> */}
         {/* <Button imageName="line" /> */}
         <input type="color" />
