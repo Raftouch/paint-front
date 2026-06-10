ea8c1057bce6d48423ff25fcf76fd52e0972ae12
raftouch <rafanelkin@gmail.com>
2026-06-10 19:47:04 +0200

    feat(tools): implement eraser drawing tool

---
+++ src/tools/Eraser.ts
@@ -0,0 +1,44 @@
+import getMousePosition from "../utils/canvas";
+import Tool from "./Tool";
+
+export default class Eraser extends Tool {
+  private isDrawing = false;
+
+  constructor(canvas: HTMLCanvasElement) {
+    super(canvas);
+    this.ctx.globalCompositeOperation = "destination-out";
+    this.listen();
+  }
+
+  listen() {
+    this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+    this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+    this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+  }
+
+  mouseUpHandler() {
+    this.isDrawing = false;
+  }
+
+  mouseDownHandler(e: MouseEvent) {
+    this.isDrawing = true;
+
+    const { x, y } = getMousePosition(this.canvas, e);
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+  }
+
+  mouseMoveHandler(e: MouseEvent) {
+    if (!this.isDrawing) return;
+
+    const { x, y } = getMousePosition(this.canvas, e);
+
+    this.draw(x, y);
+  }
+
+  draw(x: number, y: number) {
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+  }
+}
