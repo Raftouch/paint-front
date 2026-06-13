9c16c2caa0b5563f583f562d1858b1f1b08ba76b
raftouch <rafanelkin@gmail.com>
2026-06-13 19:02:38 +0200

    feat(settings): add line width control

--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -1,7 +1,17 @@
+import toolState from "../store/toolState";
+
 export default function SettingsBar() {
   return (
     <div className="h-[40px] bg-white flex items-center shadow-lg mt-2">
-      SettingsBar
+      <label htmlFor="line-width">Line width</label>
+      <input
+        onChange={(e) => toolState.setLineWidth(e.currentTarget.valueAsNumber)}
+        id="line-width"
+        type="number"
+        min={1}
+        max={50}
+        defaultValue={1}
+      />
     </div>
   );
 }
