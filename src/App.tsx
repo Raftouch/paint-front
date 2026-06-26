3c0a87234e3f83a7c73ebd88933d255e50e53102
raftouch <rafanelkin@gmail.com>
2026-06-26 15:18:42 +0200

    feat(navbar): added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import Canvas from "./components/Canvas";
 import SettingsBar from "./components/SettingsBar";
 import Toolbar from "./components/Toolbar";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-200 flex flex-col">
-      <Toolbar />
-      <SettingsBar />
-      <Canvas />
+      <Routes>
+        <Route
+          path="/:id"
+          element={
+            <>
+              <Toolbar />
+              <SettingsBar />
+              <Canvas />
+            </>
+          }
+        />
+        <Route
+          path="*"
+          element={<Navigate to={`f${(+new Date()).toString(16)}`} replace />}
+        />
+      </Routes>
     </div>
   );
 }
