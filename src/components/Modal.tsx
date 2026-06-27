a0c81d0f7c1d9030c160b15545814a9b820c1439
raftouch <rafanelkin@gmail.com>
2026-06-27 15:21:33 +0200

    feat: implement username input workflow

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,12 @@
+import type { RefObject } from "react";
+
 interface ModalProps {
+  username: RefObject<HTMLInputElement | null>;
   onClose: () => void;
+  onConnect: () => void;
 }
 
-export default function Modal({ onClose }: ModalProps) {
-  const connect = () => {};
+export default function Modal({ username, onClose, onConnect }: ModalProps) {
   return (
     <>
       <div
@@ -12,10 +15,10 @@ export default function Modal({ onClose }: ModalProps) {
       />
       <div className="fixed flex flex-col min-w-[400px] min-h-[200px] p-10 rounded bg-white text-black top-40 left-1/2 -translate-x-1/2">
         <h1 className="text-center mb-10">Type you name</h1>
-        <input type="text" />
+        <input type="text" ref={username} />
         <button
           className="px-4 py-2 bg-green-700 text-white rounded-md hover:bg-green-800 font-medium"
-          onClick={connect}
+          onClick={onConnect}
         >
           Enter
         </button>
